03a98dbb2a4120a1413b72a3ff7ce695c2186140
kiranbabugummadi <111487733+kiranbabugummadi@users.noreply.github.com>
2022-10-12 23:15:14 +0530

    t=int(input()) for i in range(t):     def convertToBinary(n):        if n > 1:            convertToBinary(n//2)        print(n % 2,end = '')
        dec =int(input())
    
        convertToBinary(dec)
        print()

---
+++ Write_a_program_to_get_the_binary_form_of_a_given_number.py
@@ -0,0 +1,11 @@
+t=int(input())
+for i in range(t):
+    def convertToBinary(n):
+       if n > 1:
+           convertToBinary(n//2)
+       print(n % 2,end = '')
+
+    dec =int(input())
+
+    convertToBinary(dec)
+    print()
