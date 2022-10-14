fe55e5a2b0c3e134ac59b00f88a9d38662504759
kiranbabugummadi <111487733+kiranbabugummadi@users.noreply.github.com>
2022-10-14 21:36:19 +0530

    n=int(input()) a=n+1 b=n-1 while a:     is_prime=True     for i in range(2,int(a**0.5)+1):         if(a%i==0):             is_prime=False             break     if is_prime==True:         break     else:         a+=1 while b:     is_prime=True     for i in range(2,int(b**0.5)+1):         if(b%i==0):             is_prime=False             break     if is_prime==True:         break     else:         b-=1
    s=0
    for i in range(1,n+1):
        if n%i==0:
            s=s+i
    if n==1 or s==n+1:
        print("0")
    elif abs(a-n)<abs(b-n):
        print(abs(a-n))
    else:  
        print(abs(b-n))

---
+++ Minimum_absolute_difference_of_a_number_and_its_closest_prime.py
@@ -0,0 +1,34 @@
+n=int(input())
+a=n+1
+b=n-1
+while a:
+    is_prime=True
+    for i in range(2,int(a**0.5)+1):
+        if(a%i==0):
+            is_prime=False
+            break
+    if is_prime==True:
+        break
+    else:
+        a+=1
+while b:
+    is_prime=True
+    for i in range(2,int(b**0.5)+1):
+        if(b%i==0):
+            is_prime=False
+            break
+    if is_prime==True:
+        break
+    else:
+        b-=1
+            
+s=0
+for i in range(1,n+1):
+    if n%i==0:
+        s=s+i
+if n==1 or s==n+1:
+    print("0")
+elif abs(a-n)<abs(b-n):
+    print(abs(a-n))
+else:  
+    print(abs(b-n))
