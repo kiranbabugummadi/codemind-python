61d86f007004b30f8a5c49c84979b2120270da35
kiranbabugummadi <111487733+kiranbabugummadi@users.noreply.github.com>
2022-10-27 21:02:27 +0530

    n=int(input()) l=list(map(int,input().split())) l1=[] c=0 if n%2==0:     for i in l:         print(i,end=" ") else:     for i in l:         print(i,end=" ")     print("0")

---
+++ Elements_with_pairs.py
@@ -0,0 +1,11 @@
+n=int(input())
+l=list(map(int,input().split()))
+l1=[]
+c=0
+if n%2==0:
+    for i in l:
+        print(i,end=" ")
+else:
+    for i in l:
+        print(i,end=" ")
+    print("0")    
