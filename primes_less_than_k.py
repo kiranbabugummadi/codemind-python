8b7428ee13ab802a5f0e3603f2748b079266942d
kiranbabugummadi <111487733+kiranbabugummadi@users.noreply.github.com>
2022-10-28 05:39:08 +0530

    def prime(x):     prime=True     for i in range(2,int(x**0.5)+1):         if x%i==0:             prime=False     if prime==True:         return 1     else:         return 0 n=int(input()) l=list(map(int,input().split())) k=int(input()) l1=[] for i in l:     if prime(i) and (i!=1) and (i<=k):         l1.append(i) print(len(l1))

---
+++ primes_less_than_k.py
@@ -0,0 +1,17 @@
+def prime(x):
+    prime=True
+    for i in range(2,int(x**0.5)+1):
+        if x%i==0:
+            prime=False
+    if prime==True:
+        return 1
+    else:
+        return 0
+n=int(input())        
+l=list(map(int,input().split()))
+k=int(input())
+l1=[]
+for i in l:
+    if prime(i) and (i!=1) and (i<=k):
+        l1.append(i)
+print(len(l1))
