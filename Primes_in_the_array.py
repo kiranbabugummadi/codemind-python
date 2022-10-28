45f78756e1a9fce05819d265aba58848f8bb8be6
kiranbabugummadi <111487733+kiranbabugummadi@users.noreply.github.com>
2022-10-28 05:50:02 +0530

    def prime(x):     prime=True     for i in range(2,int(x**0.5)+1):         if x%i==0:             prime=False     if prime==True:         return 1     else:         return 0 n=int(input()) l=list(map(int,input().split())) l1=[] for i in l:     if prime(i) and (i!=1):         l1.append(i) print(len(l1))

---
+++ Primes_in_the_array.py
@@ -0,0 +1,16 @@
+def prime(x):
+    prime=True
+    for i in range(2,int(x**0.5)+1):
+        if x%i==0:
+            prime=False
+    if prime==True:
+        return 1
+    else:
+        return 0
+n=int(input())        
+l=list(map(int,input().split()))
+l1=[]
+for i in l:
+    if prime(i) and (i!=1):
+        l1.append(i)
+print(len(l1))
