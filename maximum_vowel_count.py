1ef24665d43b0b989521d5479e26cd78ce83a145
kiranbabugummadi <111487733+kiranbabugummadi@users.noreply.github.com>
2022-10-30 13:18:36 +0530

    s=input() s1=s.lower() l=s1.split(" ") l1=[] v=['a','e','i','o','u'] for i in l:     c=0     for j in i:         if j in v:             c+=1     l1.append(c) print(max(l1))

---
+++ maximum_vowel_count.py
@@ -0,0 +1,12 @@
+s=input()
+s1=s.lower()
+l=s1.split(" ")
+l1=[]
+v=['a','e','i','o','u']
+for i in l:
+    c=0
+    for j in i:
+        if j in v:
+            c+=1
+    l1.append(c)
+print(max(l1))
